45c41275a1e70737645e87659d91a005867dbb69
Andrey Liporage <andreyliporage@pop-os.localdomain>
2021-08-09 08:59:18 -0300

    update

--- Front/src/app/pages/lista-usuarios/lista-usuarios.component.ts
+++ Front/src/app/pages/lista-usuarios/lista-usuarios.component.ts
@@ -1,24 +1,28 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Usuario } from 'src/app/Models/Usuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { BaseComponent } from '../base.component';
 
 @Component({
   selector: 'app-lista-usuarios',
   templateUrl: './lista-usuarios.component.html',
   styleUrls: ['./lista-usuarios.component.css']
 })
-export class ListaUsuariosComponent implements OnInit {
+export class ListaUsuariosComponent extends BaseComponent implements OnInit {
 
   usuarios: Usuario[] = [];
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService, protected toastr: ToastrService, protected router: Router) { super(toastr, router) }
 
   ngOnInit() {
     this.getUsuarios();
   }
 
   getUsuarios() {
-    this.usuarioService.getUsuarios().subscribe((result: Usuario[]) => this.usuarios = result);
+    this.usuarioService.getUsuarios().subscribe((result: Usuario[]) => this.usuarios = result, (falha: HttpErrorResponse) => this.processaFalha(falha));
   }
 
 }
